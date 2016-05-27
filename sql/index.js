f8c847590887bc83928cad2334da4b9256fb148c
Vitaly Tomilov <vitaly-t@users.noreply.github.com>
2016-05-28 00:21:42 +0100

    Update index.js

--- sql/index.js
+++ sql/index.js
@@ -1,1 +1,13 @@
+'use strict';
 
+var QueryFile = require('pg-promise').QueryFile;
+
+// Helper for linking to external query files;
+function sql(file) {
+    var path = './sql/' + file;
+    return new QueryFile(path, {minify: true});
+}
+
+module.exports = {
+    create: sql('create.sql')
+};
